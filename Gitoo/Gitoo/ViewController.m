236c38ae89f5673df1e2ad3822d119478475da52
userName <763815972@qq.com>
2018-12-23 23:19:12 +0800

    提交冲突

--- Gitoo/Gitoo/ViewController.m
+++ Gitoo/Gitoo/ViewController.m
@@ -19,12 +19,16 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     
+<<<<<<< HEAD
     // master
     
     // master --1
 
     // master   --2
 
+=======
+    // master 
+>>>>>>> parent of 6ad973f...     // master   --1
 }
 
 
